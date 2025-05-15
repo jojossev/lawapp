6aef566c9fdc9d4df3d33b8be83ef04967dba1e0
Cabinet Makani Marcus <contact@cabinet-makani-marcus.fr>
2025-05-15 06:35:34 +0100

    Enable error display in config.php

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,9 @@
 <?php
+// Activer l'affichage des erreurs
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Définir l'environnement
 define('ENVIRONMENT', getenv('ENVIRONMENT') ?: 'development');
 
